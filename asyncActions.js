f6584c54df5a3848ad665e8ea0a88096574ebed5
Ujjawal Joshi <ujjawaljoshi07@gmail.com>
2024-04-27 20:35:33 +0530

    FEAT: created API calls using thunk middleware

--- asyncActions.js
+++ asyncActions.js
@@ -1,5 +1,10 @@
 const redux = require('redux')
+const thunkMiddleware = require('redux-thunk').default
+const axios = require('axios')
 const createStore = redux.createStore
+const applyMiddleware = redux.applyMiddleware
+
+
 
 const initialState = {
     loading: false,
@@ -63,5 +68,24 @@ const reducer = (state = initialState, action) => {
     }
 }
 
+// action creator
+const fetchUsers = () => { // here what the thunk middleware brings to the table is that it enables this action creator to return a function instead of an action object
+    return function (dispatch) { // special things about this func:It doesn't have to be pure. It is allowed to have side effects like async API calls. ALso this func. can dispatch actions too
+        dispatch(fetchUsersRequest()) // set loading to true
+        axios.get('https://jsonplaceholder.typicode.com/users')
+            .then(response => {
+                // response.data is the users
+                const users = response.data.map(user => user.id)
+                dispatch(fetchUsersSuccess(users))
+            })
+            .catch(error => {
+                // error.message is the error
+                dispatch(fetchUsersFailure(error.message))
+            })
+    }
+}
+
 // store
-const store = createStore(reducer)
+const store = createStore(reducer, applyMiddleware(thunkMiddleware))
+store.subscribe(() => console.log('Updated state', store.getState()))
+store.dispatch(fetchUsers())
